3a6f8ec728b91bed7f822080479da3a8a8e1c929
Dmitri Akatov <akatov@gmail.com>
2013-02-05 16:39:56 -0800

    scraper probably ready

--- lib/image_scraper.rb
+++ lib/image_scraper.rb
@@ -1,5 +1,61 @@
 require "image_scraper/version"
 
+require 'nokogiri'
+require 'typhoeus'
+require 'uri'
+require 'pry'
+
 module ImageScraper
   # Your code goes here...
+  class Scraper
+    def initialize
+      @images = []
+      @urls = []
+    end
+
+    # @param [String] url
+    # @option opts [Num] :depth (0)
+    def scrape(url, opts={})
+      # puts url
+      depth = (opts.fetch :depth, 0) || 0
+      image_urls, page_urls = parse url
+      # binding.pry
+      image_urls.each do |image|
+        if @images.member? image
+          next
+        end
+        @images << image
+        puts image
+        save_image image
+      end
+      if depth > 0
+        depth = depth - 1
+        page_urls.each do |p_u|
+          if @urls.member? p_u
+            next
+          end
+          @urls << p_u
+          scrape p_u, :depth => depth
+        end
+      end
+    end
+
+    # @param [url] page
+    # @return [Array<String>, Array<String>] [image_urls, page_urls]
+    def parse(url)
+      # binding.pry
+      uri = URI.parse url
+      page = Nokogiri Typhoeus.get(url).body
+      links = (page / 'a').map{|l| l.attribute('href')}.compact.map {|a| uri.merge a.value rescue nil}.compact.map(&:to_s)
+      images = (page / 'img').map {|n| n.attribute('src')}.compact.map{|a| uri.merge a.value rescue nil}.compact.map(&:to_s)
+      [images, links]
+    end
+
+    def save_image(url)
+      r = Typhoeus.get url
+      name = url.sub(/\//, '-')
+      File.open("images/#{ name }", 'wb') { |fp| fp.write(r.body) }
+    end
+
+  end
 end
--- lib/image_scraper/cli.rb
+++ lib/image_scraper/cli.rb
@@ -1,13 +1,28 @@
 require 'thor'
-require 'pry'
-
 require 'image_scraper'
+require 'pry'
 
 module ImageScraper
   class CLI < Thor
     desc "scrape [URL]", "scrapes a URL for images"
+    method_option :depth,
+                  :aliases => '-d',
+                  :type => :numeric,
+                  :desc => 'how many links to follow'
     def scrape(url)
-      puts url
+      ImageScraper::Scraper.new.scrape url, :depth => options['depth']
+      # puts url
+      # puts options
+    end
+
+    desc "repl", "starts a repl"
+    def repl
+      binding.pry
+    end
+
+    desc "version", "returns the version"
+    def version
+      puts ImageScraper::VERSION
     end
   end
 end
